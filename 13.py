3486cb5c8c6046dd9d5756624a69ac62ed9e159c
Lim Yeon Woo <90602936+Lim-YeonWoo@users.noreply.github.com>
2023-07-04 13:44:56 -0800

    Upload 11~13번 문항

---
+++ 13.py
@@ -0,0 +1,16 @@
+'''
+#BOJ 11022 A+B-8
+
+[문제설명]
+
+[문제풀이]
+
+[어려웠던 점 & 다시 공부할 부분]
+
+'''
+
+T = int(input()) #테스트케이스 개수
+
+for i in range(T):
+    a, b = map(int, input().split())
+    print(f"Case #{i+1}: {a} + {b} = {a+b}")
